0148f983d8092626985db10ae9768b655dafd6e0
jerryjzhang <zhangjun2915@163.com>
2016-01-07 20:11:33 +0800

    update

--- start-build-tbds-docker.sh
+++ start-build-tbds-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -it --rm -w /root/tbds --entrypoint /root/tbds/build-tbds.sh -v `pwd`:/root/tbds docker.oa.com:8080/tbds/build:2.0.0-dev
+docker run -d --name tbds-build -w /root/tbds --entrypoint /root/tbds/build-tbds.sh -v `pwd`:/root/tbds docker.oa.com:8080/tbds/build:2.0.0-dev
